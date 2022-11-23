4257997d242ebf13fda80bf581470e2cc559e70d
Ilija Radovanovic <ilija009@gmail.com>
2022-11-23 12:37:02 +0100

    change footer year

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -96,7 +96,7 @@
                     <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
                         <i class="fa-brands fa-monero"></i>
                     </a>
-                    <span class="text-muted">© 2021 Company, Inc</span>
+                    <span class="text-muted">© 2022 Company, Inc</span>
                 </div>
                 <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
                     <li class="ms-3"><a class="text-muted" href="#"><i class="fa-brands fa-linkedin fa-xl"></i></a></li>
